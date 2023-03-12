234a136b1ee6422d47f4d4becc0ca62d4dd4a50a
VeraM <kalipso.v@gmail.com>
2023-03-12 23:25:44 +0300

    add homework 11

--- src/test/java/HomeworkPathThree.java
+++ src/test/java/HomeworkPathThree.java
@@ -1,7 +1,13 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeworkPathThree {
@@ -10,5 +16,17 @@ public class HomeworkPathThree {
         String phrase = "Love will save the world";//28
 
         assertTrue(phrase.length()>15, "The phase is less 15 symbols");
+    }
+
+    @Test
+    public void testEx11Cookies() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        System.out.println("\nCookies:");
+        Map<String,String> responseCookies = response.getCookies();
+        System.out.println(responseCookies);
+        assertEquals("{HomeWork=hw_value}", response.getCookies().toString(), "Unexpected cookie");
     }
 }
