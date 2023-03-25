92acd829b08a07ac3558597deaa0d82907550fa7
VeraM <kalipso.v@gmail.com>
2023-03-25 19:35:25 +0300

    all tests

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -3,6 +3,7 @@ package lib;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -11,5 +12,42 @@ public class Assertions {
 
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value isn`t equal to expected value");
+    }
+
+    public static void asserJsonByName(Response Response, String name, String expectedValue) {
+        Response.then().assertThat().body("$", hasKey(name));
+
+        String value = Response.jsonPath().getString(name);
+        assertEquals(expectedValue, value, "JSON value isn`t equal to expected value");
+    }
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text isn`t as expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+        assertEquals(
+                expectedStatusCode,
+                Response.statusCode(),
+                "Response status code isn`t as expected"
+        );
+    }
+
+    public static void assertJsonHasField(Response Response, String expectedFieldName){
+        Response.then().assertThat().body("$", hasKey(expectedFieldName));
+    }
+
+    public static void assertJsonHasFields(Response Response, String[] expectedFieldNames){
+        for (String expectedFieldName : expectedFieldNames) {
+            Assertions.assertJsonHasField(Response, expectedFieldName);
+        }
+    }
+
+    public static void assertJsonHasNotField(Response Response, String unexpectedFieldName) {
+        Response.then().assertThat().body("$", not(hasKey(unexpectedFieldName)));
     }
 }
--- src/test/java/tests/UserAuthTest.java
+++ src/test/java/tests/UserAuthTest.java
@@ -18,11 +18,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import lib.Assertions;
+import lib.ApiCoreRequests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+
+@Epic("Authorization cases")
+@Feature("Authorization")
 public class UserAuthTest extends BaseTestCase {
 
     String cookie;
     String header;
     int userIdOnAuth;
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @BeforeEach
     public void loginUser(){
@@ -30,58 +40,53 @@ public class UserAuthTest extends BaseTestCase {
         authData.put("email", "vinkotov@example.com");
         authData.put("password", "1234");
 
-        Response responseGetAuth = RestAssured
-                .given()
-                .body(authData)
-                .post("https://playground.learnqa.ru/api/user/login")
-                .andReturn();
+        Response responseGetAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
 
-        this.cookie = this.getCookie(responseGetAuth, "auth_sid"); //перед вставкой extends BaseTestCase, было после = responseGetAuth.getCookie("auth_sid");
-        this.header = this.getHeader(responseGetAuth, "x-csrf-token"); //responseGetAuth.getHeader("x-csrf-token");
-        this.userIdOnAuth = this.getIntFromJson(responseGetAuth, "user_id");//responseGetAuth.jsonPath().getInt("user_id");
+        this.cookie = this.getCookie(responseGetAuth, "auth_sid");
+        this.header = this.getHeader(responseGetAuth, "x-csrf-token");
+        this.userIdOnAuth = this.getIntFromJson(responseGetAuth, "user_id");
     }
 
     @Test
-    public void test3AuthUserWithBeforeEach() {
-        Response responseCheckAuth = RestAssured //было до импорта lib.Assertions JsonPath responseCheckAuth = RestAssured
-                .given()
-                .header("x-csrf-token", this.header)
-                .cookie("auth_sid", this.cookie)
-                .get("https://playground.learnqa.ru/api/user/auth")
-                .andReturn(); //было до импорта lib.Assertions .jsonPath();
+    @Description("This test successfully authorize user by email and password")
+    @DisplayName("Test positive auth user")
+    public void testAuthUser() {
+        Response responseCheckAuth = apiCoreRequests
+                .makeGetRequest(
+                        "https://playground.learnqa.ru/api/user/auth",
+                        this.header,
+                        this.cookie
+                );
 
-        /*int userIdOnCheck = responseCheckAuth.getInt("user_id");
-        assertTrue(userIdOnCheck > 0, "Unexpected user id");
-
-        assertEquals(
-                userIdOnAuth,
-                userIdOnCheck,
-                "User id from auth request isn`t equal to user_id from check request"
-        );*/ //было до импорта lib.Assertions
-        //появилось после импорта lib.Assertions
         Assertions.asserJsonByName(responseCheckAuth, "user_id", this.userIdOnAuth);
     }
 
+    @Description("This test checks authorization status w/o sending auth cookie or token")
+    @DisplayName("Test negative auth user")
     @ParameterizedTest
     @ValueSource(strings = {"cookie", "headers"})
-    public void test4NegativeAuthUserWithBeforeEach(String condition){
-        RequestSpecification spec = RestAssured.given();
-        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
-
-        if (condition.equals("cookie")) {
-            spec.cookie("auth_sid", this.cookie);
+    public void testNegativeAuthUser(String condition){
+        if(condition.equals("cookie")) {
+            Response responseForCheck = apiCoreRequests.makeGetRequestWithCookie(
+                    "https://playground.learnqa.ru/api/user/auth",
+                    this.cookie
+            );
+            Assertions.asserJsonByName(responseForCheck, "user_id", 0);
         } else if (condition.equals("headers")) {
-            spec.header("x-csrf-token", this.header);
+            Response responseForCheck = apiCoreRequests.makeGetRequestWithToken(
+                    "https://playground.learnqa.ru/api/user/auth",
+                    this.header
+            );
+            Assertions.asserJsonByName(responseForCheck, "user_id", 0);
         } else {
-            throw new IllegalArgumentException("Condition value is know" + condition);
+            throw new IllegalArgumentException("Condition value isn`t known: " + condition);
         }
-
-        Response responseForCheck = spec.get().andReturn();//было до импорта lib.Assertions Json responseForCheck = spec.get().jsonPath()
-        //было до импорта lib.Assertions assertEquals(0, responseForCheck.getInt("user_id"), "User_id should be 0 for unauth request");
-        Assertions.asserJsonByName(responseForCheck, "user_id", 0);
     }
 
-    @Test
+
+    //без использование метода Before
+    /*@Test
     public void test1AuthUser(){
         Map<String, String> authData = new HashMap<>();
         authData.put("email", "vinkotov@example.com");
@@ -148,5 +153,5 @@ public class UserAuthTest extends BaseTestCase {
 
         JsonPath responseForCheck = spec.get().jsonPath();
         assertEquals(0, responseForCheck.getInt("user_id"), "User_id should be 0 for unauth request");
-    }
+    }*/
 }
